50a8c4f3239a528cdc515f0eb3b3f819d65398e7
Brian Connolly <brian.connolly71@gmail.com>
2024-01-16 20:57:46 +0000

    bug in the urls

--- backend/physiofinder/api/urls.py
+++ backend/physiofinder/api/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('hello-world/', views.hello_world, name="hello-world")
+    path('', views.hello_world, name="hello_world"),
 ]
